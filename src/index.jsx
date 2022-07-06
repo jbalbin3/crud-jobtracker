5b925d79cf37ea105fce83ef02111acb25c85d59
jbalbin3 <jbalbin3@yahoo.com>
2022-07-05 21:45:20 -0700

    updated for react v18 rendering

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />)
+// ReactDOM.render(<App />, document.getElementById('app'));
